043c4ae45b6fbcfe33ed779e1c5baa7cc2bd240b
rahimliparvin <parvinvr@code.edu.az>
2023-04-19 16:00:50 -0700

    add homework

---
+++ FiorellaApp/EntityFramework-Slider/EntityFramework-Slider/Models/Category.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.Build.Framework;
+
+namespace EntityFramework_Slider.Models
+{
+    public class Category:BaseEntity
+    {
+        [Required]
+        public string Name { get; set; }
+        public ICollection<Product> Products { get; set; }
+    }
+}
---
+++ FiorellaApp/EntityFramework-Slider/EntityFramework-Slider/Models/ExpertsHeader.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Build.Framework;
+
+namespace EntityFramework_Slider.Models
+{
+    public class ExpertsHeader: BaseEntity
+    {
+        [Required]
+        public string Title { get; set; }
+
+        [Required]
+        public string Description { get; set; }
+
+    }
+}
